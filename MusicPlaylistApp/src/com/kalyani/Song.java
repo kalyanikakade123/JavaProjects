449bbb0988449fb583334f22811cbcf2a7338112
Kalyani Kakade <86308103+kalyanikakade123@users.noreply.github.com>
2023-11-17 23:46:14 +0530

    file added

---
+++ MusicPlaylistApp/src/com/kalyani/Song.java
@@ -0,0 +1,31 @@
+package com.kalyani;
+
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song() {
+
+    }
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
